1c3da78e6001ad15ccf750520914d6f37b6f2a67
chick <chick@qrhino.com>
2016-02-21 17:35:20 -0800

    make 2d test better

--- test/test_boundary_kernel.py
+++ test/test_boundary_kernel.py
@@ -15,10 +15,12 @@ class TestBoundaryStencils(unittest.TestCase):
         size = 4
 
         mesh = np.zeros([size, size, size], dtype=np.float32)
+        counter = 1.0
         for i in range(1, size - 1):
             for j in range(1, size - 1):
                 for k in range(1, size - 1):
-                    mesh[i, j, k] = 1.0
+                    mesh[i, j, k] = counter
+                    counter += 1.0
 
         print("buffer_in  is {}".format(mesh))
 
@@ -50,7 +52,7 @@ class TestBoundaryStencils(unittest.TestCase):
         out_evt.wait()
 
         print("buffer out {}".format(mesh))
-        print("linear {}".format(mesh.reshape((size**3,))))
+        # print("linear {}".format(mesh.reshape((size**3,))))
 
         print("done")
 
@@ -129,4 +131,16 @@ class TestBoundaryStencils(unittest.TestCase):
         for i in range(len(buf2)):
             print(" {:4d}".format(int(buf2[i])), end="")
         print()
+
+        expected_result = [
+          [    30,  2519,  2620,  2721,  2822,    35],
+          [  2526,    25,    26,    27,    28,  2827],
+          [  1920,    19,    20,    21,    22,  2221],
+          [  1314,    13,    14,    15,    16,  1615],
+          [   708,     7,     8,     9,    10,  1009],
+          [     0,   713,   814,   915,  1016,     5],
+        ]
+
+        self.assertEqual(mesh, expected_result)
+
         print("done")
